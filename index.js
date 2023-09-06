d7e3df8954e24fda21f4cfa6e14c11b2b812eea5
Tahmid Newaz <tahmidnewazisfar@gmail.com>
2023-09-06 22:10:55 +0600

    Removed unnecessary code from css and fixed a bug for the event listeners in the js file

--- index.js
+++ index.js
@@ -2,30 +2,28 @@ let boxes = document.querySelectorAll(".box");
 
 var current_symbol = "";
 
-function activateClcikers(){
-for (var i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener("click", function () {
-    this.textContent = current_symbol;
-    if (current_symbol === "X") {
-      current_symbol = "O";
-    } else {
-      current_symbol = "X";
-    }
-  });
-}
+function activateClcikers() {
+  for (var i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener("click", function eventHandler() {
+      this.removeEventListener("click", eventHandler);
+      this.textContent = current_symbol;
+      if (current_symbol === "X") {
+        current_symbol = "O";
+      } else {
+        current_symbol = "X";
+      }
+    });
+  }
 }
 // Target Buttons
-symbol_buttons = document.querySelectorAll('.btn');
+symbol_buttons = document.querySelectorAll(".btn");
 
 for (var i = 0; i < symbol_buttons.length; i++) {
-  
   symbol_buttons[i].addEventListener("click", function () {
     current_symbol = this.textContent;
-    document.querySelector('.button-container').style.visibility = 'hidden';
+    document.querySelector(".button-container").style.visibility = "hidden";
     activateClcikers();
   });
 }
 
 // Find Winner
-
-
