1edb48619a84edb115c9b19f38e6c5cbcd3186ff
Tahmid Newaz <tahmidnewazisfar@gmail.com>
2023-09-06 21:11:15 +0600

    Made a button option for symbol choice O/X and a visibility trigger

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let boxes = document.querySelectorAll(".box");
 
-var current_symbol = "X";
+var current_symbol = "";
 
 for (var i = 0; i < boxes.length; i++) {
   boxes[i].addEventListener("click", function () {
@@ -12,3 +12,14 @@ for (var i = 0; i < boxes.length; i++) {
     }
   });
 }
+// Target Buttons
+symbol_buttons = document.querySelectorAll('.btn');
+
+for (var i = 0; i < symbol_buttons.length; i++) {
+  
+  symbol_buttons[i].addEventListener("click", function () {
+    current_symbol = this.textContent;
+    document.querySelector('.button-container').style.visibility = 'hidden'
+  });
+}
+
