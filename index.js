50501fd9d0bdf3a736153c3c1255378332ccfd9b
Tahmid Newaz <tahmidnewazisfar@gmail.com>
2023-09-07 16:06:40 +0600

    Added winning lines for the game

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function activateClickers() {
 function eventHandler() {
   this.removeEventListener("click", eventHandler);
   this.textContent = current_symbol;
-  total_clicks ++;
+  total_clicks++;
   checkWinner(this.classList[1]);
   if (current_symbol === "X") {
     current_symbol = "O";
@@ -75,6 +75,10 @@ function checkWinner(target) {
         console.log("Winner! in Row " + (i + 1));
         disableAllListeners();
         winner_decided = true;
+        for (let j = rows[i][0]; j < rows[i][2] + 1; j++) {
+          document.querySelector(".p" + j).innerHTML +=
+          '<div class="line_1"></div>';
+        }
       } else {
         need_to_check_col = true;
       }
@@ -98,6 +102,10 @@ function checkWinner(target) {
           console.log("Winner! in Column " + (i + 1));
           disableAllListeners();
           winner_decided = true;
+          for (let j = columns[i][0]; j < columns[i][2] + 1; j += 3) {
+            document.querySelector(".p" + j).innerHTML +=
+            '<div class="line_2"></div>';
+          }
         } else {
           need_to_check_dia_1 = true;
         }
@@ -120,7 +128,13 @@ function checkWinner(target) {
     ) {
       console.log("Winner! in Diagonal 1");
       disableAllListeners();
+
       winner_decided = true;
+
+      for (let j = diagonals[0][0]; j < diagonals[0][2] + 1; j += 4) {
+        document.querySelector(".p" + j).innerHTML +=
+          '<div class="line_4"></div>';
+      }
     } else {
       need_to_check_dia_2 = true;
     }
@@ -143,15 +157,18 @@ function checkWinner(target) {
       console.log("Winner! in Diagonal 2");
       disableAllListeners();
       winner_decided = true;
+
+      for (let j = diagonals[1][0]; j < diagonals[1][2] + 1; j += 2) {
+        document.querySelector(".p" + j).innerHTML +=
+          '<div class="line_3"></div>';
+      }
     }
 
     console.log("Diagonal 2 Checked");
   }
-  if (winner_decided){
-    document.querySelector('h1').textContent = current_symbol+ " Wins!";
+  if (winner_decided) {
+    document.querySelector("h1").textContent = current_symbol + " Wins!";
+  } else if (winner_decided == false && total_clicks == 9) {
+    document.querySelector("h1").textContent = "Draw!";
   }
-  else if (winner_decided == false && total_clicks == 9){
-    document.querySelector('h1').textContent = "Draw!";
-  }
-  
 }
